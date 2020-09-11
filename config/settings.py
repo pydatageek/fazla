766c21fa012eb164c8f56c8921af0a298f75b4c5
py data geek <pydatageek@gmail.com>
2020-09-11 05:58:07 +0300

    charts.js wrong ordering issue tried to be solved

--- config/settings.py
+++ config/settings.py
@@ -117,22 +117,22 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # },
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'png04_',
-        'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'fn',
-    #     'USER': 'fn',
-    #     'PASSWORD': '123fazla!..-',
+    #     'NAME': 'png04_',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '1',
     #     'HOST': 'localhost',
     #     'PORT': '',
     # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fn',
+        'USER': 'fn',
+        'PASSWORD': '123fazla!..-',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
 }
 
 
